570660af5a6c0b50ab816b7fe098aee507312727
Andres Betancourt <andres11152@gmail.com>
2024-09-04 14:39:12 -0500

    update config, user.controller & database/connection

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+import { config } from 'dotenv';
+config();
+
+export const PORT = parseInt(process.env.PORT) || process.argv[2] || 8080;
+
+export const dbSettings = {
+    user: process.env.DB_USER || '',
+    host: process.env.DB_HOST || '',
+    database: process.env.DB_NAME || '',
+    password: process.env.DB_PW || '',
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+}
+
+export const API_KEY = process.env.API_KEY || '';
