955bbbcd715e7f6b85dc1b7529da314b446e7c36
Andres Betancourt <andres11152@gmail.com>
2024-11-10 13:51:19 -0500

    The variable id was renamed to product_id in routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.get('/api/products', getProducts);
 router.get('/api/getproduct/:product_id', getProductById);
 router.post('/api/createproduct', createProduct);
-router.put('/api/updateproduct/:id', updateProduct);
-router.delete('/api/deleteproduct/:id', deleteProduct);
+router.put('/api/updateproduct/:product_id', updateProduct);
+router.delete('/api/deleteproduct/:product_id', deleteProduct);
 
 // Endpoint para obtener la lista de precios según el tipo de usuario
 router.get('/products/prices/', getPriceList); // Agrega la ruta de precios aquí
