a505b73b10682209fdbf4580e86241043c0840d5
Andres Betancourt <andres11152@gmail.com>
2024-11-10 22:42:54 -0500

    add multer in products controller

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from '../controllers/products.controller.js';
+import multer from 'multer';
 
 const router = express.Router();
 
+// Configurar almacenamiento con multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
 router.get('/api/products', getProducts);
 router.get('/api/getproduct/:product_id', getProductById);
-router.post('/api/createproduct', createProduct);
+router.post('/api/createproduct', upload.single('photo'), createProduct);
 router.put('/api/updateproduct/:product_id', updateProduct);
 router.delete('/api/deleteproduct/:product_id', deleteProduct);
 
