7339bc79ea1853d2c584762394fa9331f3b009ea
Andres Betancourt <andres11152@gmail.com>
2024-11-25 22:53:56 -0500

    add changes in index

--- src/index.js
+++ src/index.js
@@ -14,15 +14,12 @@ import helmet from 'helmet';
 import path from 'path';
 import fs from 'fs';
 
-// Cargar variables de entorno desde el archivo .env
 dotenv.config();
 
 const app = express();
 
-// Middleware de seguridad (protección de cabeceras HTTP)
 app.use(helmet());
 
-// Middleware para redirigir a HTTPS en producción
 app.use((req, res, next) => {
   if (req.protocol !== 'https' && req.get('X-Forwarded-Proto') !== 'https') {
     return res.redirect(301, 'https://' + req.headers.host + req.url);
@@ -30,36 +27,30 @@ app.use((req, res, next) => {
   next();
 });
 
-// Middleware de logging con morgan
 if (process.env.NODE_ENV === 'production') {
-  // En producción, logueamos en un archivo de log
-  const __dirname = path.dirname(new URL(import.meta.url).pathname);  // Obtener __dirname en módulos ES
+  const __dirname = path.dirname(new URL(import.meta.url).pathname);
   const logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
-  app.use(morgan('combined', { stream: logStream })); // 'combined' para un registro más detallado
+  app.use(morgan('combined', { stream: logStream }));
 } else {
-  // En desarrollo, usamos el registro en consola
   app.use(morgan('dev'));
 }
 
-// Middleware de parseo de JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Configuración de multer para subir imágenes
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage }).single('photo');
 
-// Configuración de CORS para producción
 const allowedOrigins = [
-  'https://donkampo.com', // Dominio de producción
-  'http://localhost:3000', // Desarrollo local
-  'http://localhost:3001'  // Desarrollo local
+  'https://donkampo.com',
+  'http://localhost:3000',
+  'http://localhost:3001',
 ];
 
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);  
+      callback(null, true);
     } else {
       callback(new Error('No permitido por CORS'));
     }
@@ -71,19 +62,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Configuración del motor de plantillas
 app.set('view engine', 'ejs');
 
-// Timeout de las solicitudes
 app.use((req, res, next) => {
-  res.setTimeout(5000, () => {  
+  res.setTimeout(5000, () => {
     console.log('La solicitud ha superado el tiempo de espera.');
-    res.status(408).send('Request timed out');  
+    res.status(408).send('Request timed out');
   });
   next();
 });
 
-// Rutas de la aplicación
 app.use(authRoutes);
 app.use(usersRoutes);
 app.use(productsRoutes);
@@ -91,21 +79,24 @@ app.use(shippingRoutes);
 app.use(orderRoutes);
 app.use(customerTypesRoutes);
 
-// Ruta para crear productos (con imagen)
 app.post('/api/createproduct', upload, optimizeImage, (req, res) => {
   console.log('Imagen subida:', req.file);
   res.status(201).json({ message: 'Producto creado exitosamente' });
 });
 
-// Configuración del puerto (producción y desarrollo)
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const port = process.env.PORT || 8080;
 
-// Escuchar en el puerto 80 para entornos de producción (Render se encarga de HTTPS)
-app.listen(port, '0.0.0.0', () => {  
+app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
 
-// Manejo de señales de interrupción
 process.on("SIGINT", () => {
   console.log("Servidor cerrado correctamente");
   process.exit(0);
