3d744b24d6769bc724abf3635e290b9b4a9b480e
Andres Betancourt <andres11152@gmail.com>
2024-11-26 20:57:44 -0500

    mod allowed origins

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,68 @@
-import { config } from 'dotenv';
-config();
+import express from 'express';
+import morgan from 'morgan';
+import { PORT, allowedOrigins } from './config/config.js'; // Importa el valor de allowedOrigins
+import authRoutes from './routes/auth.routes.js';
+import usersRoutes from './routes/user.routes.js';
+import productsRoutes from './routes/products.routes.js';
+import shippingRoutes from './routes/shipping.routes.js';
+import orderRoutes from './routes/order.routes.js';
+import customerTypesRoutes from './routes/customerTypes.routes.js';
+import multer from 'multer';
+import { optimizeImage } from './middlewares/imageMiddleware.js';
+import cors from 'cors';
 
-export const PORT = process.env.PORT || 8080;
+const app = express();
 
-export const dbSettings = {
-    user: process.env.DB_USER || '',
-    host: process.env.DB_HOST || '',
-    database: process.env.DB_NAME || '',
-    password: process.env.DB_PW || '',
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-}
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-export const API_KEY = process.env.API_KEY || '';
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage }).single('photo');
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.set('view engine', 'ejs');
+
+app.use((req, res, next) => {
+  res.setTimeout(5000, () => {
+    console.log('La solicitud ha superado el tiempo de espera.');
+    res.status(408).send('Request timed out');
+  });
+  next();
+});
+
+app.use(authRoutes);
+app.use(usersRoutes);
+app.use(productsRoutes);
+app.use(shippingRoutes);
+app.use(orderRoutes);
+app.use(customerTypesRoutes);
+
+app.post('/api/createproduct', upload, optimizeImage, (req, res) => {
+  console.log('Imagen subida:', req.file);
+  res.status(201).json({ message: 'Producto creado exitosamente!' });
+});
+
+app.listen(PORT, () => {
+  const host = `http://localhost:${PORT}`;
+  console.log(`Servidor corriendo en: ${host}`);
+});
+
+process.on("SIGINT", () => {
+  console.log("Servidor cerrado correctamente");
+  process.exit(0);
+});
