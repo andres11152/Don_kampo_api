937ca2eb564e7138aaca4af732e1e35c1cdd9a6d
Andres Betancourt <andres11152@gmail.com>
2024-12-16 18:42:52 -0500

    del dist

--- dist/routes/shipping.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createShippingInfo, getShippingInfo, getShippingInfoById, updateShippingInfo, deleteShippingInfo } from '../controllers/shipping.controller.js';
-const router = express.Router();
-router.post('/api/createshipping', createShippingInfo);
-router.get('/api/shipping', getShippingInfo);
-router.get('/api/getshipping/:id', getShippingInfoById);
-router.put('/api/updateshipping/:id', updateShippingInfo);
-router.delete('/api/deleteshipping/:id', deleteShippingInfo);
-export default router;
