36f2706e155b87838c69dbb15d542cac5eaba785
Andres Betancourt <andres11152@gmail.com>
2025-01-30 09:27:05 -0500

    fix babel dependency

--- src/controllers/advertisements.controller.js
+++ src/controllers/advertisements.controller.js
@@ -64,7 +64,7 @@ export const createAdvertisement = async (req, res) => {
       related_product_id || null, // Si no se proporciona, se establece como null
     ]);
 
-    const advertisementId = result.rows[0] ? result.rows[0].advertisement_id : undefined;
+    const advertisementId = result.rows[0]?.advertisement_id;
 
     res.status(201).json({
       message: 'Publicidad creada exitosamente',
